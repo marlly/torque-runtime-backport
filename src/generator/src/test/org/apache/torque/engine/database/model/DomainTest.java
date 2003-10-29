608e693dff75251a6c7370b924cacc25827ef92f
Martin Poeschl <mpoeschl@marmot.at>
2003-10-29 01:19:51 +0000

    add tests for domain handling
    git-svn-id: http://svn.apache.org/repos/asf/db/torque/trunk@229303 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/generator/src/test/org/apache/torque/engine/database/model/DomainTest.java
@@ -0,0 +1,120 @@
+package org.apache.torque.engine.database.model;
+
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ *    "Apache Torque" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    "Apache Torque", nor may "Apache" appear in their name, without
+ *    prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+import junit.framework.TestCase;
+
+import org.apache.torque.engine.database.transform.XmlToAppData;
+
+/**
+ * Tests for domain handling.
+ *
+ * @author <a href="mailto:mpoeschl@marmot.at>Martin Poeschl</a>
+ * @version $Id$
+ */
+public class DomainTest extends TestCase
+{
+    private XmlToAppData xmlToAppData = null;
+    private Database db = null;
+
+    public DomainTest(String name)
+    {
+        super(name);
+    }
+
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        xmlToAppData = new XmlToAppData("mysql", "defaultpackage");
+        db = xmlToAppData.parseFile(
+            "src/test/org/apache/torque/engine/database/model/domaintest-schema.xml");
+    }
+
+    protected void tearDown() throws Exception
+    {
+        xmlToAppData = null;
+        super.tearDown();
+    }
+
+    /**
+     * test if the tables get the package name from the properties file
+     */
+    public void testAllAttributes() throws Exception
+    {
+        Domain amount = db.getDomain("amount");
+        assertEquals("NUMERIC", amount.getType());
+        assertEquals("10", amount.getSize());
+        assertEquals("2", amount.getScale());
+        assertEquals("0", amount.getDefaultValue());
+        assertEquals("amount domain", amount.getDescription());
+    }
+    
+    /**
+     * test if the tables get the package name from the properties file
+     */
+    public void testDomainColumn() throws Exception
+    {
+        Table table = db.getTable("product");
+        Column name = table.getColumn("name");
+        assertEquals("VARCHAR", name.getType());
+        assertEquals("40", name.getSize());
+        Column price = table.getColumn("price");
+        assertEquals("NUMERIC", price.getType());
+        assertEquals("10", price.getSize());
+//        assertEquals("2", price.getScale());
+        assertEquals("0", price.getDefaultValue());
+    }
+    
+}
